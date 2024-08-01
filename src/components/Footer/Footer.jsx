c274ec60eccc4ce00489ae6c4a5049933b04fff0
pranavagarthade <pagarthade@gmail.com>
2024-08-01 13:22:42 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Footer.css';
+import Youtube from '../../assets/youtube.png';
+import Instagram from '../../assets/instagram.png';
+import LinkedIn from '../../assets/linkedin.png';
+import Logo from '../../assets/logo.png';
+
+const Footer = () => {
+  return (
+    <div className='Footer-container'>
+      <hr />
+      <div className='footer'>
+        <div className='social-links'>
+          <img src={Youtube} alt='YouTube'/>
+          <img src={Instagram} alt='Instagram'/>
+          <img src={LinkedIn} alt='LinkedIn'/>
+        </div>
+        <div className='logo-f'>
+          <img src={Logo} alt='Logo' />
+        </div>
+      </div>
+      <div className='blur blur-f-1'></div>
+      <div className='blur blur-f-2'></div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/data/testimonialsData.js
@@ -0,0 +1,76 @@
+// import image1 from "../assets/t-image1.png";
+// import image2 from "../assets/t-image2.jpg";
+// import image3 from "../assets/t-image3.jpg";
+import woman from"../assets/t-woman.jpg";
+import man from"../assets/man.jpg";
+
+
+export const testimonialsData = [
+  {
+    image: woman,
+    review:
+      "Hey, I am Ridima. When I joined, I was 70.5 kg, and today I am 65.8 kg. From XL, I have come to M size. Seriously, I am very lucky to have you in my life. Along with body toning, my mind toning is done automatically. Thank you so much for making me meet myself.",
+    name: 'Ridima',
+    // status : 'ENTREPRENEUR'
+  },
+  {
+    image: man,
+    review: 'Hey Gita, thanks to you, my diabetes levels are now normal, and I feel more energized with less sleep. The doctor reduced my medicines by 50%, and I am experiencing positive changes in various aspects of my health. Joining your class was a life-changing decision. Thank you! 🙂 ',
+    name: 'Kumar',
+    // status: 'COACH'
+  },
+  {
+    image : woman,
+    review:'Achieved great results from June to October, going from 72 kg to 59 kg, all thanks to Geetanjali Kandale. I am so fit now that people are often surprised when they see me. Maintaining a healthy diet and regular exercise is crucial for my well-being. Never neglect your mental health.',
+    name: 'Sonal',
+    // status: "CUSTOMER"
+  },
+
+  {
+    image: man,
+    review:
+      "Hey, I am Harshal. Geetanjali, your guidance has transformed my life. With normalized diabetes levels, increased energy on less sleep, and a 50% reduction in medication, I owe it all to your classes. It's been a life-changing decision, and I can't thank you enough! 🙂",
+    name: 'Harshal',
+    // status : 'ENTREPRENEUR'
+  },
+  {
+    image: woman,
+    review: 'Hey, I am Ruplai. I am following the diet plan, and you said to update my weight on Friday. Today is Thursday, and I am 66.80 kg 😍😍, very very happy. With my doctors permission, I have been taking half a BP tablet for the last 8 days, and my BP is normal. Thank you so much for making me meet myself',
+    name: 'Rupali',
+    // status: 'COACH'
+  },
+  {
+    image : man,
+    review:'Joined the class last year weighing 67 kg and lost 10 kg in a year. Weight remains constant now. After a 2-month break, still no weight gain. In Geetas class for 1.5 years, solved many problems. Gained muscle strength, energy, overall happiness, and health.',
+    name: 'Raman',
+    // status: "CUSTOMER"
+  },
+
+  {
+    image: woman,
+    review:
+      "Hey, I am Pornima. When I joined, I was 70.5 kg, and today I am 65.8 kg. From XL size, I have come to M size. Seriously, I am very lucky to have you in our life. Along with body toning, my mind toning is done automatically. Thank you so much for making me meet myself.",
+    name: 'Pornima',
+    // status : 'ENTREPRENEUR'
+  },
+  {
+    image: woman,
+    review: 'Hey, I am Mauri. I visited my doctor, and he is pleased with my progress. He suggested reducing my Tyronorm dosage from 50 to 25. We will monitor for three months and adjust based on the blood test results. Grateful for the positive changes happening for me. ',
+    name: 'Mauri',
+    // status: 'COACH'
+  },
+  {
+    image : woman,
+    review:'Hey, I am Dipti. Hi Mam, I visited my doctor, and he is very happy. He said to adjust my Tyronorm dosage to 25 instead of 50. We’ll observe for 3 months and make necessary changes based on the blood test results. Thanks, Mam 🙏🏻. I am very happy everything is working together for me 😊.',
+    name: 'Dipti',
+    // status: "CUSTOMER"
+  },
+
+  {
+    image: woman,
+    review:
+      "Hey, I am Riddhi. Good morning! I've been diligently following the diet plan. According to your advice, I update my weight on Fridays, but today is Thursday. With my doctor's approval, I’ve been taking half of the prescribed blood pressure tablet, and my blood pressure is now normal. 👍",
+    name: 'Riddhi',
+    // status : 'ENTREPRENEUR'
+  }
+];

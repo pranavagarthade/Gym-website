9519f346156a3f13346c935c57b58d14e714af9c
prath2305 <prathameshc2305@gmail.com>
2024-08-05 20:23:45 +0530

    ChavanEdits

--- src/components/Programs/HealthFitnes.jsx
+++ src/components/Programs/HealthFitnes.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HealthFitnes = () => {
     return (
     <div>
-      <h1>Welcome to my React page!</h1>
+      <h1>HEALTHFITNESS</h1>
       <p>This is a simple React page.</p>
     </div>
   );
